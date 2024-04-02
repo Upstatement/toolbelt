ec82e8d912d773cc90976b020460e6f2723ddeed
gvjacob <gvjacob@outlook.com>
2024-04-01 22:40:26 -0400

    Configure build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,39 +1,24 @@
-let fs = require('fs')
-//CDN
-build({
-    entryPoints: [`builds/cdn.js`],
-    outfile: `dist/plugin.min.js`,
-    bundle: true,
-    minify: true,
-    sourcemap: false,
-    platform: 'browser',
-    define: { CDN: true },
-})
-//Module
-build({
-    entryPoints: [`builds/module.js`],
-    outfile: `dist/plugin.esm.js`,
-    bundle: true,
-    bundle: true,
-    platform: 'neutral',
-    mainFields: ['main', 'module'],
-})
-build({
-    entryPoints: [`builds/module.js`],
-    outfile: `dist/plugin.cjs.js`,
-    bundle: true,
-    target: ['node10.4'],
-    platform: 'node',
-})
+const esbuild = require("esbuild");
 
-
-function build(options){
-    options.define || (options.define = {})
-    options.define['process.env.NODE_ENV'] = process.argv.includes('--watch') ? `'production'` : `'development'`
-
-    return require('esbuild').build({
-        watch: process.argv.includes('--watch'),
-        // external: ['alpinejs'],
-        ...options,
-    }).catch(() => process.exit(1))
+async function build(options) {
+  return esbuild.build(options).catch(() => process.exit(1));
 }
+
+// Minified for CDN
+build({
+  entryPoints: [`builds/cdn.js`],
+  outfile: `dist/plugin.min.js`,
+  bundle: true,
+  minify: true,
+  sourcemap: false,
+  platform: "browser",
+});
+
+// ES Module
+build({
+  entryPoints: [`builds/module.js`],
+  outfile: `dist/plugin.esm.js`,
+  bundle: true,
+  platform: "neutral",
+  mainFields: ["main", "module"],
+});
