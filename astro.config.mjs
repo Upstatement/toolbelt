706e2f9cf2f53623f31c00a3ebeab665056d9b46
gvjacob <gvjacob@outlook.com>
2024-04-03 08:39:52 -0400

    Install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -12,13 +14,18 @@ export default defineConfig({
       sidebar: [
         {
           label: "Getting Started",
-          autogenerate: { directory: "getting-started" },
+          autogenerate: {
+            directory: "getting-started",
+          },
         },
         {
           label: "Components",
-          autogenerate: { directory: "components" },
+          autogenerate: {
+            directory: "components",
+          },
         },
       ],
     }),
+    tailwind(),
   ],
 });
