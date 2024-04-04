4fbe53debbe5916b4d70cda72aeeecdea8391170
gvjacob <gvjacob@outlook.com>
2024-04-04 02:12:11 -0400

    Create Tailwind palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,26 @@ import starlightPlugin from "@astrojs/starlight-tailwind";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: {
+          200: "#aec8ff",
+          600: "#1a51ff",
+          900: "#0c2877",
+          950: "#0c1f50",
+        },
+        gray: {
+          100: "#f2f6ff",
+          200: "#e5eeff",
+          300: "#b3c3df",
+          400: "#6f8bc1",
+          500: "#3e5789",
+          700: "#1f3665",
+          800: "#0e2451",
+          900: "#0d182c",
+        },
+      },
+    },
   },
   plugins: [starlightPlugin()],
 };
