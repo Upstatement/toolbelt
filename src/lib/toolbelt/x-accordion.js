43383fffbc79037ae31acdd89290018ea58e0c8f
gvjacob <gvjacob@outlook.com>
2024-04-05 17:19:05 -0400

    Create x-accordion

---
+++ src/lib/toolbelt/x-accordion.js
@@ -0,0 +1,132 @@
+import logger from "../logger";
+import { isElementTag } from "../utils";
+
+/**
+ * "x-accordion" is a vertically stacked set of interactive headings
+ * that each reveal an associated section of content.
+ *
+ * @param {import('alpinejs').Alpine} Alpine
+ */
+export default function(Alpine) {
+  Alpine.directive("accordion", (el, { value }) => {
+    if (value === "item") {
+      handleItem(el, Alpine);
+    } else if (value === "trigger") {
+      handleTrigger(el, Alpine);
+    } else if (value === "content") {
+      handleContent(el, Alpine);
+    } else {
+      handleRoot(el, Alpine);
+    }
+  });
+}
+
+/**
+ * @param {HTMLElement} el
+ * @param {import('alpinejs').Alpine} Alpine
+ */
+function handleRoot(el, Alpine) {
+  Alpine.bind(el, {
+    "x-data"() {
+      return {
+        __root: true,
+      };
+    },
+  });
+}
+
+/**
+ * @param {HTMLElement} el
+ * @param {import('alpinejs').Alpine} Alpine
+ */
+function handleItem(el, Alpine) {
+  Alpine.bind(el, {
+    "x-data"() {
+      return {
+        open: false,
+        __item: true,
+      };
+    },
+
+    "x-init"() {
+      if (!this.__root) {
+        logger.error(
+          "x-accordion:item must be placed inside an x-accordion",
+          el,
+        );
+      }
+    },
+
+    "x-id"() {
+      return ["toolbelt-accordion-trigger", "toolbelt-accordion-content"];
+    },
+  });
+}
+
+/**
+ * @param {HTMLElement} el
+ * @param {import('alpinejs').Alpine} Alpine
+ */
+function handleTrigger(el, Alpine) {
+  Alpine.bind(el, {
+    "x-init"() {
+      if (!this.__item) {
+        logger.error(
+          "x-accordion:trigger must be placed inside an x-accordion:item",
+          el,
+        );
+      }
+
+      if (!isElementTag(el, "button")) {
+        logger.error("x-accordion:trigger must be a <button> element", el);
+      }
+    },
+
+    ":id"() {
+      return this.$id("toolbelt-accordion-trigger");
+    },
+
+    ":aria-controls"() {
+      return this.$id("toolbelt-accordion-content");
+    },
+
+    ":aria-expanded"() {
+      return this.open;
+    },
+
+    "@click"() {
+      this.open = !this.open;
+    },
+  });
+}
+
+/**
+ * @param {HTMLElement} el
+ * @param {import('alpinejs').Alpine} Alpine
+ */
+function handleContent(el, Alpine) {
+  Alpine.bind(el, {
+    "x-init"() {
+      if (!this.__item) {
+        logger.error(
+          "x-accordion:content must be placed inside an x-accordion:item",
+          el,
+        );
+      }
+    },
+
+    ":id"() {
+      return this.$id("toolbelt-accordion-content");
+    },
+
+    role: "region",
+
+    ":aria-labelledby"() {
+      return this.$id("toolbelt-accordion-trigger");
+    },
+
+    "x-show"() {
+      return this.open;
+    },
+  });
+}
