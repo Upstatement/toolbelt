d05d679d2dc98283c2ae87ee37f9d58d07d7bf5e
gvjacob <gvjacob@outlook.com>
2024-04-06 16:49:48 -0400

    gst

---
+++ src/lib/toolbelt/x-tabs.js
@@ -0,0 +1,154 @@
+import logger from "../logger";
+import { isElementTag } from "../utils";
+
+/**
+ * `x-tabs` is a set of layered sections of content known
+ * as tab panels that are displayed one at a time.
+ *
+ * @param {import('alpinejs').Alpine} Alpine
+ */
+export default function(Alpine) {
+  Alpine.directive("tabs", (el, { value }) => {
+    if (value === "list") {
+      handleList(el, Alpine);
+    } else if (value === "trigger") {
+      handleTrigger(el, Alpine, { value });
+    } else if (value === "content") {
+      handleContent(el, Alpine, { value });
+    } else {
+      handleRoot(el, Alpine);
+    }
+  });
+}
+
+/**
+ * @param {HTMLElement} el
+ * @param {import('alpinejs').Alpine} Alpine
+ * @param {{ defaultTab: string }} config
+ */
+function handleRoot(el, Alpine, config) {
+  Alpine.bind(el, {
+    "x-data"() {
+      return {
+        __root: true,
+        activeTab: config.defaultTab || null,
+      };
+    },
+
+    "x-id"() {
+      return ["tabs-trigger", "tabs-content"];
+    },
+  });
+}
+
+/**
+ * @param {HTMLElement} el
+ * @param {import('alpinejs').Alpine} Alpine
+ */
+function handleList(el, Alpine) {
+  Alpine.bind(el, {
+    "x-init"() {
+      if (!this.__root) {
+        logger.warn("x-tabs:trigger must be placed inside an x-tabs.", el);
+      }
+    },
+
+    "x-data"() {
+      return {
+        __list: true,
+      };
+    },
+
+    role: "tablist",
+
+    tabindex: 0,
+  });
+}
+
+/**
+ * @param {HTMLElement} el
+ * @param {import('alpinejs').Alpine} Alpine
+ * @param {{ value: string }} config
+ */
+function handleTrigger(el, Alpine, config) {
+  if (!config.value) {
+    return logger.error("x-tabs:trigger must have a value.", el);
+  }
+
+  Alpine.bind(el, {
+    "x-init"() {
+      if (!this.__list) {
+        logger.warn("x-tabs:trigger must be placed inside an x-tabs:list.", el);
+      }
+
+      if (!isElementTag(el, "button")) {
+        logger.warn("x-tabs:trigger must be a <button> element.", el);
+      }
+    },
+
+    "x-data"() {
+      return {
+        value: config.value,
+      };
+    },
+
+    ":id"() {
+      return `${this.$id("tabs-trigger")}-${this.value}`;
+    },
+
+    role: "tab",
+
+    tabindex() {
+      return this.activeTab === this.value ? 0 : -1;
+    },
+
+    ":aria-selected"() {
+      return this.activeTab === this.value;
+    },
+
+    ":aria-controls"() {
+      return `${this.$id("tabs-content")}-${this.value}`;
+    },
+  });
+}
+
+/**
+ * @param {HTMLElement} el
+ * @param {import('alpinejs').Alpine} Alpine
+ * @param {{ value: string }} config
+ */
+function handleContent(el, Alpine, config) {
+  if (!config.value) {
+    return logger.error("x-tabs:content must have a value.", el);
+  }
+
+  Alpine.bind(el, {
+    "x-init"() {
+      if (!this.__root) {
+        logger.warn("x-tabs:content must be placed inside an x-tabs", el);
+      }
+    },
+
+    "x-data"() {
+      return {
+        value: config.value,
+      };
+    },
+
+    ":id"() {
+      return `${this.$id("tabs-content")}-${this.value}`;
+    },
+
+    role: "tabpanel",
+
+    tabindex: 0,
+
+    ":aria-labelledby"() {
+      return `${this.$id("tabs-trigger")}-${this.value}`;
+    },
+
+    "x-show"() {
+      return this.activeTab === this.value;
+    },
+  });
+}
