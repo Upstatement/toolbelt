d8a92e341e720c814d555e12cbc2641a65f9e0f9
gvjacob <gvjacob@outlook.com>
2024-04-06 07:51:19 -0400

    Docs for accordion

--- src/lib/toolbelt/index.js
+++ src/lib/toolbelt/index.js
@@ -1,3 +1,4 @@
+import accordion from "./x-accordion";
 import dialog from "./x-dialog";
 import section from "./x-section";
 import link from "./x-link";
@@ -6,6 +7,7 @@ import link from "./x-link";
  * @param {import('alpinejs').Alpine} Alpine
  */
 export default function(Alpine) {
+  accordion(Alpine);
   section(Alpine);
   link(Alpine);
   dialog(Alpine);
