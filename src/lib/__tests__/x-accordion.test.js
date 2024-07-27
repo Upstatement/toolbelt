22b5922770b2d117cc4a6be66ccae50d728d8273
gvjacob <gvjacob@outlook.com>
2024-07-27 00:25:53 -0400

    test for opening a tab

--- src/lib/__tests__/x-accordion.test.js
+++ src/lib/__tests__/x-accordion.test.js
@@ -1,5 +1,5 @@
 import { Alpine } from "alpinejs";
-import { getByTestId } from "@testing-library/dom";
+import { fireEvent, getByTestId, waitFor } from "@testing-library/dom";
 import "@testing-library/jest-dom";
 
 import toolbelt from "../toolbelt";
@@ -37,6 +37,41 @@ describe("x-accordion", () => {
       expect(content).toHaveAttribute("aria-labelledby", trigger.id);
       expect(content).toHaveAttribute("role", "region");
       expect(content).toHaveAttribute("data-state", "closed");
+    });
+
+    describe("opening a tab", () => {
+      test("x-accordion:item should be open", async () => {
+        const item = getByTestId(document, "item-1");
+        const trigger = getByTestId(document, "trigger-1");
+
+        fireEvent.click(trigger);
+
+        await waitFor(() => {
+          expect(item).toHaveAttribute("data-state", "open");
+        });
+      });
+
+      test("x-accordion:trigger should be open", async () => {
+        const trigger = getByTestId(document, "trigger-1");
+
+        fireEvent.click(trigger);
+
+        await waitFor(() => {
+          expect(trigger).toHaveAttribute("data-state", "open");
+          expect(trigger).toHaveAttribute("aria-expanded", "true");
+        });
+      });
+
+      test("x-accordion:content should be open", async () => {
+        const trigger = getByTestId(document, "trigger-1");
+        const content = getByTestId(document, "content-1");
+
+        fireEvent.click(trigger);
+
+        await waitFor(() => {
+          expect(content).toHaveAttribute("data-state", "open");
+        });
+      });
     });
   });
 });
