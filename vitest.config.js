86ab43af456f6a5d178cd1cead941faf5a4d4bfd
gvjacob <gvjacob@outlook.com>
2024-07-27 11:57:36 -0400

    Specify vitest include

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   test: {
+    include: ["./src/lib/__tests__/**/*.test.js"],
     environment: "jsdom",
     setupFiles: ["./src/lib/__tests__/setup.js"],
   },
