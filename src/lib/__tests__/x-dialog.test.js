69f975f7c9317937c0cfce374fe0610ad9ae6e7e
gvjacob <gvjacob@outlook.com>
2024-07-27 09:49:49 -0400

    Initial state test for dialog

---
+++ src/lib/__tests__/x-dialog.test.js
@@ -0,0 +1,49 @@
+import { getByTestId } from "@testing-library/dom";
+import "@testing-library/jest-dom";
+
+import { html, initializeAlpine } from "./utils";
+
+describe("x-dialog", () => {
+  beforeAll(initializeAlpine);
+
+  describe("default configuration", () => {
+    beforeEach(() => {
+      document.body.innerHTML = html`
+        <div x-dialog>
+          <button x-dialog:trigger data-testid="toggle"></button>
+
+          <template x-teleport="body">
+            <div x-dialog:content data-testid="content">
+              <div x-dialog:overlay data-testid="overlay"></div>
+              <h2 x-dialog:title data-testid="title"></h2>
+              <p x-dialog:description data-testid="description"></p>
+              <button x-dialog:close data-testid="close"></button>
+            </div>
+          </template>
+        </div>
+      `;
+    });
+
+    test("correct initial state", () => {
+      const toggle = getByTestId(document.body, "toggle");
+      const content = getByTestId(document.body, "content");
+      const title = getByTestId(document.body, "title");
+      const description = getByTestId(document.body, "description");
+      const close = getByTestId(document.body, "close");
+
+      expect(toggle).toHaveAttribute("aria-expanded", "false");
+      expect(toggle).toHaveAttribute("data-state", "closed");
+      expect(toggle).toHaveAttribute("aria-haspopup", "dialog");
+      expect(toggle).toHaveAttribute("aria-controls", content.id);
+
+      expect(content).toHaveAttribute("role", "dialog");
+      expect(content).toHaveAttribute("tabindex", "-1");
+      expect(content).toHaveAttribute("aria-modal", "true");
+      expect(content).toHaveAttribute("aria-labelledby", title.id);
+      expect(content).toHaveAttribute("aria-describedby", description.id);
+      expect(content).toHaveAttribute("data-state", "closed");
+
+      expect(close).toBeInTheDocument();
+    });
+  });
+});
