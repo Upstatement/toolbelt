e2c071b5b50ca9d4cf550fb47e1b6d0d37ddf0dd
gvjacob <gvjacob@outlook.com>
2024-12-14 20:03:06 -0500

    Placeholder logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,11 @@ export default defineConfig({
     starlight({
       title: "Toolbelt",
 
+      logo: {
+        src: "./src/assets/toolbelt.svg",
+        replacesTitle: true,
+      },
+
       social: {
         github: "https://github.com/upstatement/toolbelt",
       },
